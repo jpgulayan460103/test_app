ac8ad1ed3aecb0440ff1f31eac5ecbea1840fc69
John Paul <jpgulayan@gmail.com>
2020-10-11 11:48:22 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.dark}>
+      <Header />
+        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ScrollView>
+          <Text category='h1'>Hello World</Text>
+        </ScrollView>
+        </Layout>
+      </ApplicationProvider>
+    </SafeAreaView>
+  );
+}
+
+export default App;
